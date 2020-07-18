0ffdfe518da0c980a075f1961e7b386b9228e4a8
Kiriyama <57665042+AvishekPD@users.noreply.github.com>
2020-07-18 18:56:24 +0530

    today I learned C is magic have so much datatypes

---
+++ data-types/not-to-run.c
@@ -0,0 +1,21 @@
+//Datatypes the Basics one
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char theChar;
+    unsigned char theCharU;
+
+    int theInt;
+    unsigned int theIntU;
+    long int theLongInt;
+    unsigned long int theLongIntU;
+
+    float theFloat;
+    double theDouble;
+    long double theLongDouble;
+    unsigned long double theLongDoubleU;
+}
